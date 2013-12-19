2a9f04bb5bae55e5cd5def874d44a35398c92332
Bertrand Delacretaz <bdelacretaz@apache.org>
2013-12-19 21:56:33 +0100

    formatting

--- src/test/java/ch/x42/osgi/avro/AvroSerializationIT.java
+++ src/test/java/ch/x42/osgi/avro/AvroSerializationIT.java
@@ -26,37 +26,35 @@ import ch.x42.osgi.avro.generated.AvroTestUser;
 public class AvroSerializationIT {
 
 	private final Random random = new Random(System.currentTimeMillis());
-	public static final String BUNDLE_FILENAME ="bundle.filename";
-	
+	public static final String BUNDLE_FILENAME = "bundle.filename";
+
 	@Inject
 	private AvroSerializer serializer;
-	
-    @Configuration
-    public Option[] config()
-    {
-    	final File bundleFile = new File(System.getProperty(BUNDLE_FILENAME, "BUNDLE_FILENAME_NOT_SET"));
-    	
-        return options(
-            junitBundles(),
-            bundle(bundleFile.toURI().toString()),
-            mavenBundle("org.apache.felix", "org.apache.felix.scr", "1.6.2")
-        );
-    }
-    
-    @Test
-    public void testSerialization() {
-    	final AvroTestUser u = new AvroTestUser();
-    	u.setName("NAME_" + random.nextInt());
-    	u.setFavoriteNumber(random.nextInt());
 
-    	assertEquals("Expecting equals to work", u, u);
-    	
-		final byte [] data = serializer.serialize(u);
+	@Configuration
+	public Option[] config() {
+		final File bundleFile = new File(System.getProperty(BUNDLE_FILENAME, "BUNDLE_FILENAME_NOT_SET"));
+
+		return options(
+				junitBundles(),
+				bundle(bundleFile.toURI().toString()),
+				mavenBundle("org.apache.felix", "org.apache.felix.scr", "1.6.2"));
+	}
+
+	@Test
+	public void testSerialization() {
+		final AvroTestUser u = new AvroTestUser();
+		u.setName("NAME_" + random.nextInt());
+		u.setFavoriteNumber(random.nextInt());
+
+		assertEquals("Expecting equals to work", u, u);
+
+		final byte[] data = serializer.serialize(u);
 		assertNotNull("Expecting non-null data", data);
 		assertTrue("Expecting non-empty data", data.length > 0);
-		
+
 		final AvroTestUser copy = serializer.deserialize(data);
-        assertNotNull("Expecting non-null copy", copy);
-        assertEquals("Expecting copy to match", u, copy);
-    }
+		assertNotNull("Expecting non-null copy", copy);
+		assertEquals("Expecting copy to match", u, copy);
+	}
 }
